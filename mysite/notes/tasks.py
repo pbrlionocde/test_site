174d1ca538975077835925eb29876f9ec72076d6
pbrlionocde <gorbachenkomisha@gmail.com>
2021-04-16 15:33:16 +0000

    update send message

--- mysite/notes/tasks.py
+++ mysite/notes/tasks.py
@@ -1,16 +1,20 @@
-#from django.template import Template, Context
+from notes.models import Note, User
 from django.conf import settings
 from django.core.mail import send_mail
 from mysite.celery import app
-#from notes.models import Note, User
 
 
 @app.task
 def send_not_end_notes():
-    send_mail(
-            'Your QuickPublisher Activity',
-            'text_email',
-            settings.EMAIL_HOST_USER,
-            ['gorbachenkomisha@gmail.com'],
-            fail_silently=False,
-        )
+    for user in User.objects.all():
+        notes = Note.objects.all().not_done()
+        tasks = ''
+        for note in notes:
+            tasks += note.text_note + ' ' + str(note.date_of_creation) + '\n'
+        send_mail(
+                'Your QuickPublisher Activity',
+                'Hello!It`s your not done tasks: {}'.format(tasks),
+                settings.EMAIL_HOST_USER,
+                [user.email],
+                fail_silently=False,
+            )
